87be348c077f82852f5eedca0d709b643c60601e
ericktheredd5875 <ericktheredd5875@gmail.com>
2025-05-13 17:29:53 -0700

    chore: update make file for using render hosting.

--- prod.sh
+++ prod.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Lets get the migrate CLI..."
+curl -L https://github.com/golang-migrate/migrate/releases/download/v4.18.3/migrate.linux-amd64.tar.gz | tar xvz
+sudo mv migrate /usr/local/bin/
 
 echo "Migrating database..."
 migrate -path ./db/migrations -database "$(DATABASE_URL)" -verbose force 1 || true
