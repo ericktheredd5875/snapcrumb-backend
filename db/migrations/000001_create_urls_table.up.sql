69a47cec79716f8d80341ac874516899144546cc
ericktheredd5875 <ericktheredd5875@gmail.com>
2025-05-13 18:02:56 -0700

    fix: updated the sql for urls table.

--- db/migrations/000001_create_urls_table.up.sql
+++ db/migrations/000001_create_urls_table.up.sql
@@ -1,6 +1,10 @@
 CREATE TABLE urls (
-  id SERIAL PRIMARY KEY,
-  short_code TEXT UNIQUE NOT NULL,
-  original_url TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  id integer NOT NULL DEFAULT nextval('urls_id_seq'::regclass),
+  original_url text COLLATE pg_catalog."default" NOT NULL,
+  shortcode character varying(10) COLLATE pg_catalog."default" NOT NULL,
+  created_at timestamp without time zone DEFAULT now(),
+  expires_at timestamp without time zone,
+  visit_count integer DEFAULT 0,
+  CONSTRAINT urls_pkey PRIMARY KEY (id),
+  CONSTRAINT urls_shortcode_key UNIQUE (shortcode)
 );
