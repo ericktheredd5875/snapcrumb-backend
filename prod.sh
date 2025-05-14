18835212139a53a9f27d75ad3d7233687e8c7bef
ericktheredd5875 <ericktheredd5875@gmail.com>
2025-05-13 17:33:26 -0700

    chore: update make file for using render hosting.

--- prod.sh
+++ prod.sh
@@ -2,11 +2,11 @@
 
 echo "Lets get the migrate CLI..."
 curl -L https://github.com/golang-migrate/migrate/releases/download/v4.18.3/migrate.linux-amd64.tar.gz | tar xvz
-sudo mv migrate /usr/local/bin/
+mv migrate /usr/local/bin/
 
 echo "Migrating database..."
-migrate -path ./db/migrations -database "$(DATABASE_URL)" -verbose force 1 || true
-migrate -path ./db/migrations -database "$(DATABASE_URL)" -verbose up
+migrate -path ./db/migrations -database "$DATABASE_URL" -verbose force 1 || true
+migrate -path ./db/migrations -database "$DATABASE_URL" -verbose up
 
 echo "Starting SnapCrumb in Production mode using Render..."
 ./snapcrumb
