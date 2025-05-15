cfa8ab30f30c163726ffd5ca4433e409fdb38ad4
ericktheredd5875 <ericktheredd5875@gmail.com>
2025-05-15 12:12:10 -0700

    debug: testing file path traversing in gitflow and render

--- pkg/utils/findProjectRoot_test.go
+++ pkg/utils/findProjectRoot_test.go
@@ -1,13 +1,17 @@
 package utils
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFindProjectRoot(t *testing.T) {
+
+	rootDir := os.Getenv("ASSERT_ROOT_DIR")
+
 	root, err := FindProjectRoot("")
 	assert.NoError(t, err)
-	assert.Equal(t, "C:/CodeBases/snapcrumb-backend", root)
+	assert.Equal(t, rootDir, root)
 }
